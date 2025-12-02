b1209bbd9adc9a776b7452b85bd069aea2f1c53b
Tairesu <tyresecook25@yahoo.com>
2025-12-02 02:13:46 -0600

    init Project level urls

--- scraphounds/urls.py
+++ scraphounds/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from scraphounds import views as project_views
+
 
 urlpatterns = [
+    path('', project_views.root_view, name='home_urlpattern'),
     path('admin/', admin.site.urls),
 ]
