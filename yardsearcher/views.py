515e6309e8e94dadb1887fe156139d4710dd739d
Tairesu <tyresecook25@yahoo.com>
2025-12-05 16:18:19 -0600

    results_view(): Passing Dummy Data -> results.html

--- yardsearcher/views.py
+++ yardsearcher/views.py
@@ -9,5 +9,17 @@ def results_view(request):
 	context = {}
 	if request.method == "GET":
 		context['query'] = request.GET.get('q') or " "
+		context['fetched_yard_data'] = [
+      		{
+				"name" : "Joliet U-Pull It",
+				"elem_id" : "jap",
+				"num_results" : 0,
+				"time_elapsed" : 1.3,
+				"result_headers" : ("make","model","year","date entered","stk"),
+				"results" : [
+        			("Honda", "Civic", 1977, "11/22/2000", 234243),
+           		],
+            },
+      ]
 
 	return render(request, 'yardsearcher/results.html', context)
