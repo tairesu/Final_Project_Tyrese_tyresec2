760f4fe6066fbf25c910ae33b1a8bbd85b4d8711
Tairesu <tyresecook25@yahoo.com>
2025-12-08 01:43:02 -0600

    jup: mirrored headers from site

--- yardsearcher/utils/jup.py
+++ yardsearcher/utils/jup.py
@@ -176,6 +176,24 @@ class Jup(YardSearch):
         super().__init__(query_str)
         self.name = "Joliet U-Pull It"
         self.elem_id = "jap"
+        self.headers = {
+            "authority": "www.jolietupullit.com",
+            "method": "GET",
+            "scheme": "https",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-encoding": "gzip, deflate, br, zstd",
+            "accept-language": "en-US,en;q=0.9","priority": "u=0, i",
+            "referer": "https://www.jolietupullit.com/inventory/",
+            "sec-ch-ua": '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"macOS"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+        }
 
     def fetch_inventory_html_soup(self,conditionals):
         make = conditionals['make'].upper()
