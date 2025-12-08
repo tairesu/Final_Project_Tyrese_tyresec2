fd26a9df3dfa015b41fd5dd97b11045c29296a02
Tairesu <tyresecook25@yahoo.com>
2025-12-08 12:28:26 -0600

    lkq: refactored and init convert car to tuple

--- yardsearcher/utils/lkq.py
+++ yardsearcher/utils/lkq.py
@@ -21,17 +21,22 @@ class LKQSearch(YardSearch):
             querys (str): The raw search query 
         """
         super().__init__(query)
-        selfname = "LKQ Blue Island"
-        self.base_url = "https://www.lkqpickyourpart.com/DesktopModules/pyp_vehicleInventory/getVehicleInventory.aspx"
+        self.name = "LKQ Blue Island"
+        self.base_url = "https://www.pyp.com/DesktopModules/pyp_vehicleInventory/getVehicleInventory.aspx"
         self.base_headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             "Accept": "*/*",
-            "Referer": "https://www.lkqpickyourpart.com/inventory/blue-island-1582/",
+            "Referer": "https://www.pyp.com/inventory/blue-island-1582/",
             "X-Requested-With": "XMLHttpRequest",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "en-US,en;q=0.9"
         }
         self.base_params = {}
+        self.temp_inventory_headers = []
+        self.inventory_headers = ()
+        self.lat = 41.6325570
+        self.long = -87.6731408
+        self.elem_id = "lkq"
 
     def fetch_inventory(self, store_id=1582, conditionals={}):
         page_number = 1
@@ -43,18 +48,17 @@ class LKQSearch(YardSearch):
 
     def is_page_valid(self, page_number,store_id, conditionals={}) -> bool:
         # Grabs all divs HTML 
-        vehicle_cards = self.fetch_inventory_html(page_number, query, store_id, conditionals)
+        vehicle_cards = self.fetch_inventory_html(page_number, store_id, conditionals)
         return len(vehicle_cards) > 0
 
 
-    def fetch_inventory_html(self, page_number,query,store_id, conditionals={}) -> list:
-        self.params = {
+    def fetch_inventory_html(self, page_number,store_id, conditionals={}) -> list:
+        self.base_params = {
             "page": page_number,
-            "filter": query,
+            "filter": conditionals['original_query'],
             "store": store_id
         }
-        response = requests.get(self.base_url, headers=self.headers, params=self.params)
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = super().fetch_inventory(conditionals)
         vehicle_cards = soup.find_all(class_="pypvi_resultRow")
         if len(vehicle_cards) > 1:
             self.handle_vehicle_cards_html(vehicle_cards, conditionals)
@@ -64,7 +68,7 @@ class LKQSearch(YardSearch):
     def handle_vehicle_cards_html(self, vehicle_cards=[], conditionals={}):
         for card in vehicle_cards:
             vehicle_data = self.extract_card_html(card)
-            if self.satisfies_conditionals(vehicle_data, conditionals):
+            if super().satisfies_conditionals(vehicle_data, conditionals):
                 self.results.append(vehicle_data)
 
 
@@ -79,18 +83,34 @@ class LKQSearch(YardSearch):
         #loop through each detail item (color, stock, available inventory_car, etc)
         for detail in details:
             items = detail.find_all('b')
-            if len(items) > 1:
+            if len(items) > 0:
                 for item in items:
                     field_name = item.get_text(strip=True).replace(':','').lower().strip()
-                    inventory_car[field_name] = item.next_sibling.strip()
-            elif len(items) == 1:
-                field_name = items[0].get_text().replace(':','').lower().strip()
-                item_value = items[0].next_sibling.strip()
-                if('available' in field_name):
-                    item_value = detail.find('time').get_text()
-                inventory_car[field_name] = item_value
-        return inventory_car
+                    self.temp_inventory_headers.append(field_name)
+                    if('available' in field_name):
+                        item_value = detail.find('time').get_text()
+                    else: 
+                        item_value = item.next_sibling.strip()
+                    inventory_car[field_name] = item_value
 
+  
+        return self.convert_car_to_tuple(inventory_car)
+
+    def convert_car_to_tuple(self, inventory_car):
+        self.inventory_headers = tuple(inventory_car.keys())
+        return (
+            inventory_car['year'], 
+            inventory_car['make'],
+            inventory_car['model'],
+            inventory_car['color'],
+            inventory_car['vin'],
+            inventory_car['section'],
+            inventory_car['row'],
+            inventory_car['space'],
+            inventory_car['stock #'],
+            inventory_car['available'],
+        )
+        
 
     def display_data(self):
         if len(self.results) == 0:
