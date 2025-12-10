a8e22d6bd21898a5003b88c114492c18ab3b6358
Tairesu <tyresecook25@yahoo.com>
2025-12-10 01:21:41 -0600

    Initialized Vehicle Model

--- yardsearcher/admin.py
+++ yardsearcher/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from yardsearcher.models import (
-    Junkyard
+    Junkyard,
+    Vehicle,
 )
+
+
 # Register your models here.
 admin.site.register(Junkyard)
+admin.site.register(Vehicle)
