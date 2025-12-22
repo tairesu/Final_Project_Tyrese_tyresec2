9d39ecc03f2ea93ebbfbcc7be1e12d92f3989fd9
tyroPatel <tyresecook25@yahoo.com>
2025-12-22 02:41:04 -0600

    fixed lats print statement

--- yardsearcher/views.py
+++ yardsearcher/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponse, JsonResponse
 from yardsearcher.utils.jup import *
 from yardsearcher.utils.lkq import *
+from django.views.generic import View
 
 def results_view(request):
 	"""
@@ -12,14 +13,16 @@ def results_view(request):
 		query = request.GET.get('q') or " "
 		fetched_yard_data = []
 
+		# Grab data from various inventories based on query 
 		jup_search = Jup(query)
 		jup_search.handle_queries()
 		fetched_yard_data.append(jup_search.data_as_dict())
 		lkq_blue_search = LKQSearch(query)
 		lkq_blue_search.handle_queries()
 		fetched_yard_data.append(lkq_blue_search.data_as_dict())
+
 		print(f"view longs: {YardSearch.longs}")
-		print(f"view longs: {YardSearch.lats}")
+		print(f"view lats: {YardSearch.lats}")
 		n = len(YardSearch.longs)
 		avg_lat = sum(YardSearch.lats) / n
 		avg_long = sum(YardSearch.longs) / n
@@ -29,3 +32,4 @@ def results_view(request):
 		context['avg_long'] = avg_long
 
 	return render(request, 'yardsearcher/results.html', context)
+
