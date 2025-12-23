0293f3d4e9478c07e99e73cf8c9ddd3ff9c0402d
tyroPatel <tyresecook25@yahoo.com>
2025-12-23 00:53:48 -0600

    refresh command upserts instead of deletes

--- yardsearcher/management/commands/refresh_inventories.py
+++ yardsearcher/management/commands/refresh_inventories.py
@@ -18,23 +18,23 @@ class Command(BaseCommand):
 	current_yard = ''
 
 	def handle(self, *args, **options):
+		#Vehicle.objects.all().deletebulk update or create django ()
 		self.stdout.write("Starting inventory refresh...")
 		for yard in ALLOWED_YARDS.keys():
 			yard_class = ALLOWED_YARDS[yard]['class']("")
-			self.stdout.write(f"Processing {yard_class.name}")
 			yard_class.handle_queries()
+			self.stdout.write(f"Processing {yard_class.name}")
 			self.current_yard = yard
 			self.process_inventory(yard_class.results_as_list(), ALLOWED_YARDS[yard]['id'])
 
 	def process_inventory(self, results_list, yard_id):
 		assert len(results_list) > 0
-		print(results_list)
 		models_list = []
 		for car_dict in results_list:
 			year = car_dict['year']
 			make = car_dict['make']
 			model = car_dict['model']
-			row = car_dict['vehicle row'] if 'vehicle_row' in car_dict.keys() else 0
+			row = car_dict['vehicle row'] if 'vehicle row' in car_dict.keys() else 0
 			space = car_dict['space'] if 'space' in car_dict.keys() else 0
 			color = car_dict['color'] if 'color' in car_dict.keys() else ""
 			junkyard_indentifier = car_dict['stock#'] if 'stock#' in car_dict.keys() or 'stock #' in car_dict.keys() else ""
@@ -42,7 +42,7 @@ class Command(BaseCommand):
 			available_date = self.extract_date(car_dict)
 			models_list.append(Vehicle(junkyard_id=yard_id, year=year, make=make, model=model, available_date=available_date, row=row, space=space, color=color, junkyard_indentifier=junkyard_indentifier, vin=vin))
 
-		print(models_list)
+		Vehicle.objects.bulk_create(models_list, update_conflicts=True, unique_fields=['junkyard_indentifier','junkyard'], update_fields=['year','make','model'])
 
 	def extract_date(self, car_dict):
 		date = ''
