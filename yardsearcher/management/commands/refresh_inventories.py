9936e12e3b973f3079b7fb0dfff12d002537594e
tyroPatel <tyresecook25@yahoo.com>
2025-12-23 22:45:15 -0600

    refresh command has been fragmented for readability

--- yardsearcher/management/commands/refresh_inventories.py
+++ yardsearcher/management/commands/refresh_inventories.py
@@ -12,6 +12,14 @@ ALLOWED_YARDS = {
 		'date_format': '%m.%d.%y'
 	},
 }
+ALLOWED_YARDS2 = [
+	{
+		'name': 'Joliet U-Pull-It',
+		'class': Jup,
+		'id': 1,
+		'date_format': '%m.%d.%y',
+	},
+]
 
 class Command(BaseCommand):
 	help = "Refreshes junkyard and vehicle data"
@@ -19,86 +27,101 @@ class Command(BaseCommand):
 	current_yard_id = 0
 
 	def handle(self, *args, **options):
-		self.stdout.write("Starting inventory refresh...")
-		# Pipe the inventory results of each scraper into process_inventory
-		for yard in ALLOWED_YARDS.keys():
-			self.current_yard = yard
-			self.current_yard_id = ALLOWED_YARDS[yard]['id']
-			yard_class = ALLOWED_YARDS[yard]['class']("") # Init scraper class
-			yard_class.handle_queries()
-			self.stdout.write(f"Processing {yard_class.name}")
-			self.process_inventory(yard_class.results_as_list())
-			self.stdout.write(self.style.SUCCESS(f"Successfully refreshed {yard} inventory!"))
-
-	def process_inventory(self, results_list ):
 		""" 
-			Format results from results_list to populate the database 
+			Initializes all junkyard scrapers and upserts their results into Vehicle models 
+			(Also deletes vehicles that are physically removed from the junkyard )
+
 		"""
-		assert len(results_list) > 0
+		for junkyard in ALLOWED_YARDS2:
+			self.stdout.write(f"Refreshing {junkyard['name']}")
+			scraper = junkyard['class']("")
+			scraper.handle_queries()
+			self.cache_scraper_results(scraper.results_as_list(), junkyard)
+			self.stdout.write(self.style.SUCCESS(f"\nSuccessfully refreshed {junkyard['name']}'s inventory!\n"))
+
+	def cache_scraper_results(self, results, junkyard_meta):
+		""" 
+			Upserts or deletes vehicles on Vehicle model 
+		"""
+		assert len(results) > 0
 		models_list = []
-		# Will capture identifiers from scraped results: ['stk0192','stk1111']
-		scraped_identifiers = []
+		scraped_identifiers = [] # Ex: ['stk0192','stk1111']
 
-		for car_dict in results_list:
-			year = car_dict['year']
-			make = car_dict['make']
-			model = car_dict['model']
+		# Format results to django model instances (<Vehicle>) 
+		for result in results:
+			vehicle_instance = self.format_result(result, junkyard_meta)
+			scraped_identifiers.append(vehicle_instance.junkyard_identifier)
+			models_list.append(vehicle_instance)
+		self.upsert_models_list(models_list)
+		self.handle_removed_results(scraped_identifiers)
 
-			# some keys may require further extraction
-			# a jup key may be 'stock#' while lkq 'stock #'  
-			row = self.extract_row(car_dict)
-			space = self.extract_space(car_dict)
-			color = self.extract_color(car_dict)
-			junkyard_indentifier = self.extract_junkyard_identifier(car_dict)
-			vin = self.extract_vin(car_dict)
-			available_date = self.extract_date(car_dict)
-			scraped_identifiers.append(junkyard_indentifier)
-			models_list.append(Vehicle(junkyard_id=self.current_yard_id, year=year, make=make, model=model, available_date=available_date, row=row, space=space, color=color, junkyard_indentifier=junkyard_indentifier, vin=vin))
+	def format_result(self, result, junkyard_meta):
+		"""
+		Turns any scraped result into an instance of the <Vehicle> model
+		"""
+		year = result['year']
+		make = result['make']
+		model = result['model']
 
-		Vehicle.objects.bulk_create(models_list, update_conflicts=True, unique_fields=['junkyard_indentifier','junkyard'], update_fields=['year','make','model'])
-		different_identifiers = Vehicle.objects.exclude(junkyard_indentifier__in=scraped_identifiers)
+		# some result keys require more handling because junkyard inventory column headers differ
+		# Ex: JUP uses 'vehicle_row' while LKQ's may say 'row'  
+		row = self.extract_row(result)
+		junkyard_identifier = self.extract_junkyard_identifier(result)
+		color = self.extract_color(result)
+		space = self.extract_space(result)
+		available_date = self.extract_date(result, junkyard_meta['date_format'])
+		vin = self.extract_vin(result)
+		return Vehicle(junkyard_id=junkyard_meta['id'], year=year, make=make, model=model, available_date=available_date, row=row, space=space, color=color, junkyard_identifier=junkyard_identifier, vin=vin)
 
-		print(f"{scraped_identifiers}")
-		print(f"'{len(different_identifiers)}Vehicles that are no longer there: '{different_identifiers}")
-		different_identifiers.delete()
-
-	def extract_junkyard_identifier(self, car_dict):
-		junkyard_identifier = ""
-		if 'stock#' in car_dict.keys():
-			junkyard_identifier = car_dict['stock#']
-		elif 'stock #' in car_dict.keys():
-			junkyard_identifier = car_dict['stock #']
-		return junkyard_identifier
-
-	def extract_color(self, car_dict):
-		color = ""
-		if 'color' in car_dict.keys():
-			color = car_dict['color']
-		return color
-
-	def extract_row(self, car_dict):
+	def extract_row(self, result):
 		row = 0
-		if 'vechicle_row' in car_dict.keys():
-			row = car_dict['vechicle_row']
+		if 'vechicle_row' in result.keys():
+			row = result['vechicle_row']
 		return row
 
-	def extract_space(self, car_dict):
+	def extract_junkyard_identifier(self, result):
+		junkyard_identifier = ""
+		if 'stock#' in result.keys():
+			junkyard_identifier = result['stock#']
+		elif 'stock #' in result.keys():
+			junkyard_identifier = result['stock #']
+		return junkyard_identifier
+
+	def extract_color(self, result):
+		color = ""
+		if 'color' in result.keys():
+			color = result['color']
+		return color
+
+	def extract_space(self, result):
 		space = 0
-		if 'space' in car_dict.keys():
-			space = car_dict['space']
+		if 'space' in result.keys():
+			space = result['space']
 		return space
 
-	def extract_date(self, car_dict):
+	def extract_date(self, result, date_format):
 		date = ''
-		yard_date_format = ALLOWED_YARDS[self.current_yard]['date_format']
-		if 'date set in yard' in car_dict.keys():
-			date = car_dict['date set in yard']
-		elif 'available' in car_dict.keys():
-			date = car_dict['available'] 
-		return datetime.strptime(date, yard_date_format)
+		if 'date set in yard' in result.keys():
+			date = result['date set in yard']
+		elif 'available' in result.keys():
+			date = result['available'] 
+		return datetime.strptime(date, date_format)
 
-	def extract_vin(self, car_dict):
+	def extract_vin(self, result):
 		vin = ""
-		if 'vin' in car_dict.keys():
-			vin = car_dict['vin']
+		if 'vin' in result.keys():
+			vin = result['vin']
 		return vin
+
+	def upsert_models_list(self, models_list):
+		# Upsert list of <Vehicle> instances
+		Vehicle.objects.bulk_create(models_list, update_conflicts=True, unique_fields=['junkyard_identifier','junkyard'], update_fields=['year','make','model'])
+		self.stdout.write(self.style.SUCCESS(f"\t- Upserted {len(models_list)} Vehicles"))
+
+	def handle_removed_results(self, scraped_identifiers):
+		different_identifiers = Vehicle.objects.exclude(junkyard_identifier__in=scraped_identifiers)
+		different_identifiers.delete()
+		if len(different_identifiers) > 0:
+			self.stdout.write(f"\t- Deleting {len(different_identifiers)} vehicles: \n {different_identifiers}")
+		else:
+			pass
