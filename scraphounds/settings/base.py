5b71fbac14474d55b8d020a28a49ce4d7bb53d00
tyroPatel <tyresecook25@yahoo.com>
2026-01-16 22:49:33 -0600

    init 3 setting configs

--- scraphounds/settings/base.py
+++ scraphounds/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'scraphounds.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
---
+++ scraphounds/settings/production.py
@@ -0,0 +1,4 @@
+from scraphounds.settings.base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
