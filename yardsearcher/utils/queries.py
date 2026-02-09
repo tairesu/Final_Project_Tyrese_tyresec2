ecedf3f69af68ffdac3ef77e9a9516f35d26c6eb
Tairesu <tyresecook25@yahoo.com>
2026-02-08 19:17:04 -0600

    utils.queries parse_car_year_range uses dynamic year prefix

--- yardsearcher/utils/queries.py
+++ yardsearcher/utils/queries.py
@@ -33,7 +33,7 @@ def extract_conditionals(query="") -> dict:
         conditionals['year'] = parse_car_year(query)
         year = re.findall("^\d{2}\s|^\d{4}\s|\s\d{2}\s|\s\d{4}\s|\s\d{4}$|\s\d{2}$", query)[0].strip()
         semantics.pop(semantics.index(year))
-    elif not is_year_present(query) and is_year_range_present(query):
+    elif is_year_range_present(query):
         conditionals['minYear'], conditionals['maxYear'] = parse_car_year_range(query)
         year_range = re.findall("\d+\-\d+", query)[0]
         semantics.pop(semantics.index(year_range))
@@ -80,8 +80,10 @@ def parse_car_year_range(query="") -> tuple:
     range_str = re.findall(r"^[0-9]{4}-[0-9]{4}\s|\s[0-9]{4}-[0-9]{4}|^[0-9]{2}-[0-9]{2}\s|\s[0-9]{2}-[0-9]{2}", query.strip())[0]
     min_year = range_str.split('-')[0].strip()
     max_year = range_str.split('-')[1].strip()
-    formatted_min_year = "20" + min_year if len(min_year) == 2 else min_year 
-    formatted_max_year = "20" + max_year if len(max_year) == 2 else max_year 
+    min_year_prefix = get_year_prefix(min_year)
+    max_year_prefix = get_year_prefix(max_year)
+    formatted_min_year = min_year_prefix + min_year 
+    formatted_max_year = max_year_prefix + max_year
 
     return (formatted_min_year,formatted_max_year)
 
