e040b643d3dcc2441b7815ee5cd89638e2464ab7
Tairesu <tyresecook25@yahoo.com>
2026-02-15 00:49:48 -0600

    registered sort table api to urls

--- yardsearcher/api-urls.py
+++ yardsearcher/api-urls.py
@@ -4,9 +4,10 @@ URL configuration for scraphounds project.
 """
 from django.urls import path, include
 from yardsearcher.views import (
-    api_test_json_response
+    api_test_json_response,
+    api_sort_table
 )
 
 urlpatterns = [
-    path('api/sortTable/', api_test_json_response, name="results_urlpattern"),
+    path('api/sortTable/', api_sort_table, name="api_sort_table_urlpattern"),
 ]
