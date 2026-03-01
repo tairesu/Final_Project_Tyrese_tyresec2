80b4ae42b68b1ab5f7ec27821fde3f1c49aec497
Tairesu <tyresecook25@yahoo.com>
2026-03-01 17:32:07 -0600

    Init JS for ratings icons

---
+++ static/js/ratings.js
@@ -0,0 +1,15 @@
+const ratings = document.querySelectorAll('#ratings svg');
+let current_rating = 0;
+let hovering_rating = 0;
+
+function update_ratings(new_rating){
+    console.log(new_rating)
+}
+
+function hover_prior_ratings(new_rating){
+    console.log(new_rating)
+}
+
+function unhover_prior_ratings(){
+    console.log(new_rating)
+}
